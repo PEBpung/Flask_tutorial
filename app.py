c67f61f15f4b221e279850bb8e91df5a478da55a
PEBpung <mcuwwa29@gmail.com>
2021-07-12 23:10:29 +0900

    [Feat] database

--- app.py
+++ app.py
@@ -1,10 +1,51 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+# ///는 상대 경로, ////는 절대 경로
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
-@app.route('/')
+class Todo(db.Model):
+    # DB의 유형 정의, 기본키 True
+    id = db.Column(db.Integer, primary_key=True)
+    # 작업을 유지, 200자를 적당한 값으로 설정, 공백은 False
+    content = db.Column(db.String(200), nullable=False)
+    # 생성된 날짜, 기본을 UTC로 설정
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+        #return f'<Task {self.id}>'
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    if request.method =='POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your task'
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+    
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+
+    except:
+        return 'There was a problem deleting your task'
 
 if __name__ == "__main__":
     app.run(debug=True)
