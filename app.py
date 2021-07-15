74044bba710ca5829f2eb938925194901c56f118
PEBpung <mcuwwa29@gmail.com>
2021-07-15 11:14:29 +0900

    [Complete] Flask App

--- app.py
+++ app.py
@@ -16,12 +16,11 @@ class Todo(db.Model):
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
-        return '<Task %r>' % self.id
-        #return f'<Task {self.id}>'
+        return f'<Task {self.id}>'
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    if request.method =='POST':
+    if request.method == 'POST':
         task_content = request.form['content']
         new_task = Todo(content=task_content)
 
@@ -31,10 +30,12 @@ def index():
             return redirect('/')
         except:
             return 'There was an issue adding your task'
+
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
-    
+
+
 @app.route('/delete/<int:id>')
 def delete(id):
     task_to_delete = Todo.query.get_or_404(id)
@@ -43,9 +44,25 @@ def delete(id):
         db.session.delete(task_to_delete)
         db.session.commit()
         return redirect('/')
-
     except:
-        return 'There was a problem deleting your task'
+        return 'There was a problem deleting that task'
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task.content = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue updating your task'
+
+    else:
+        return render_template('update.html', task=task)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
